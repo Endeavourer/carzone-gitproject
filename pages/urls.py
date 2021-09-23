c92f930be97c9a02aec6f22a5ebb833459592c1b
Endeavourer <abhiyankraj11@gmail.com>
2021-09-23 15:36:05 +0530

    till dynamic navigation link

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
 ]
