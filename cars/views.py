249adcdcaff5779570bca26e4143ba6857c9593a
Endeavourer <abhiyankraj11@gmail.com>
2022-01-09 13:23:44 +0530

    dedicated cars page

--- cars/views.py
+++ cars/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Car
 # Create your views here.
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = Car.objects.order_by('created_date')
+    data = {
+        'cars' : cars,
+    }
+    return render(request, 'cars/cars.html', data)
 
 def car_detail(request, id):
     single_car = get_object_or_404(Car,pk=id)
