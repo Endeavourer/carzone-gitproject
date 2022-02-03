b575fe798f2fe312ee5700978cc3132e95bceb1c
Endeavourer <abhiyankraj11@gmail.com>
2022-02-03 21:16:52 +0530

    cars

--- cars/urls.py
+++ cars/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.cars, name='cars'),
-    path('<int:id>',views.car_detail,name='car_detail')
+    path('<int:id>',views.car_detail,name='car_detail'),
+    path('search', views.search, name='search')
 ]
--- cars/views.py
+++ cars/views.py
@@ -18,3 +18,7 @@ def car_detail(request, id):
         'single_car': single_car,
     }
     return render(request, 'cars/car_detail.html',data)
+
+
+def search(request):
+    return render(request,'cars/search.html')
