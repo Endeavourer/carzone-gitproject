2061b62ed25fe862d42eae7a5ab5ac89949c5453
Endeavourer <abhiyankraj11@gmail.com>
2022-03-24 00:28:26 +0530

    Fix Search Issues

--- cars/views.py
+++ cars/views.py
@@ -22,10 +22,38 @@ def car_detail(request, id):
 
 def search(request):
     cars = Car.objects.order_by('created_date')
+
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
         if keyword:
             cars = cars.filter(description__icontains = keyword)
+
+    if 'model' in request.GET:
+        model = request.GET['model']
+        if model:
+            cars = cars.filter(model__iexact = model)
+
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            cars = cars.filter(city__iexact = city)
+
+    if 'year' in request.GET:
+        year = request.GET['year']
+        if year:
+            cars = cars.filter(year__iexact = year)
+
+    if 'body_style' in request.GET:
+        body_style = request.GET['body_style']
+        if body_style:
+            cars = cars.filter(body_style__iexact = body_style)
+
+    if 'min_price' in request.GET:
+        min_price = request.GET['min_price']
+        max_price = request.GET['max_price']
+        if max_price:
+            cars = cars.filter(price__gte=min_price, price__lte=max_price)
+
     data = {
         'cars' : cars,
     }
